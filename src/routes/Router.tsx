bb0372a229b4b90494d55199029e672c76e9b7fd
humbertojr <astronomi@gmail.com>
2025-02-14 17:45:40 -0300

    Updated Routers

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,11 +1,16 @@
-import * as React from 'react';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 
-import { AppStack } from './AppStack';
-import { AuthStack } from './AuthStack';
+import { AuthStack } from "./AuthStack";
+import { AppDrawer } from "./AppDrawer";
+import { useAuth } from "../contexts/Auth";
 
 export function Router() {
-    const auth = false;
-  return (
-    auth ? <AppStack /> : <AuthStack />
-  );
+	const { authData } = useAuth();
+
+	return (
+		<NavigationContainer>
+			{authData ? <AppDrawer /> : <AuthStack />}
+		</NavigationContainer>
+	);
 }

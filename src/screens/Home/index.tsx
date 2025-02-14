7b94ebae93f3b4ce6835322fb4aaad61e715be23
humbertojr <astronomi@gmail.com>
2025-02-14 17:47:55 -0300

    Created Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,21 +1,29 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import { CustomButton } from "../../components/CustomButton";
 
 export function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
-     </View>
-  );
+	const navigation = useNavigation();
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Home Screen</Text>
+			<CustomButton
+				title='Settings'
+				onPress={() => navigation.navigate("Setting")}
+			/>
+		</View>
+	);
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#000',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    title: {color: '#fff', fontSize: 22},
+	container: {
+		flex: 1,
+		backgroundColor: "#000",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	title: { color: "#fff", fontSize: 22 },
 });

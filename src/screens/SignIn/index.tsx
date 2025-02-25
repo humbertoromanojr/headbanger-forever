e289774461febb288f1e783ec91628c9bb98a3ba
humbertojr <astronomi@gmail.com>
2025-02-25 20:58:02 -0300

    Updating SignIn screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,57 +1,71 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Alert, ActivityIndicator } from "react-native";
+import {
+	View,
+	StyleSheet,
+	Alert,
+	ActivityIndicator,
+	SafeAreaView,
+} from "react-native";
 
 import { CustomButton } from "../../components/CustomButton";
 import { CustomInput } from "../../components/CustomInput";
 import { useAuth } from "../../contexts/Auth";
 export function SignInScreen() {
-	const { signIn, isLoading } = useAuth();
+	const { signIn } = useAuth();
 
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
+	const [isLoading, setIsLoading] = React.useState(false);
+
 	async function handleAccessCredential() {
 		try {
+			setIsLoading(true);
+
 			if (!password.trim() || !email.trim()) {
 				return Alert.alert("Inscrição", "Preencha todos os campos!!!");
 			}
 			console.log("------ SignIn ok ----- ", signIn);
 			signIn(email, password);
+			setIsLoading(false);
 		} catch (error) {
 			Alert.alert("Ingresso", "Não foi possível encontrá-lo!");
+			setIsLoading(false);
 			console.error("==> SignIn: ", error);
 		}
 	}
 
 	return (
-		<View style={styles.container}>
-			<CustomInput
-				title='Email'
-				placeholder='Email'
-				placeholderTextColor='#fff'
-				value={email}
-				keyboardType='default'
-				autoCapitalize='none'
-				onChangeText={setEmail}
-			/>
-			<CustomInput
-				title='Senha'
-				placeholderTextColor='#fff'
-				placeholder='Password'
-				secureTextEntry
-				value={password}
-				onChangeText={setPassword}
-			/>
-			{isLoading ? (
-				<ActivityIndicator size={"large"} color='#fff' />
+		<SafeAreaView style={styles.container}>
+			{!isLoading ? (
+				<>
+					<CustomInput
+						title='Email'
+						placeholder='Email'
+						placeholderTextColor='#fff'
+						value={email}
+						keyboardType='default'
+						autoCapitalize='none'
+						onChangeText={setEmail}
+					/>
+					<CustomInput
+						title='Senha'
+						placeholderTextColor='#fff'
+						placeholder='Password'
+						secureTextEntry
+						value={password}
+						onChangeText={setPassword}
+					/>
+					<CustomButton
+						onPress={() => handleAccessCredential()}
+						title='Register'
+						isLoading={isLoading}
+					/>
+				</>
 			) : (
-				<CustomButton
-					onPress={() => handleAccessCredential()}
-					title='Register'
-					isLoading={isLoading}
-				/>
+				<ActivityIndicator size={"large"} color='#fff' />
 			)}
-		</View>
+		</SafeAreaView>
 	);
 }
 

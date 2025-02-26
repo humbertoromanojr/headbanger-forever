102ea3c988173d333fb143f141bbf22eece2cbcf
humbertojr <astronomi@gmail.com>
2025-02-25 21:04:06 -0300

    Created CustomDrawer component navigation

---
+++ src/routes/CustomDrawer.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+	DrawerContentScrollView,
+	DrawerItemList,
+} from "@react-navigation/drawer";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
+
+import { useAuth } from "../contexts/Auth";
+
+export function CustomDrawerContent(props, navigation) {
+	const { signOut } = useAuth();
+
+	function closeDrawer() {
+		props.navigation.closeDrawer();
+	}
+
+	return (
+		<DrawerContentScrollView style={styles.container} {...props}>
+			<View style={styles.headerContainer}>
+				<TouchableOpacity onPress={closeDrawer}>
+					<Icon
+						name='close'
+						size={30}
+						color={"#ccc"}
+						style={{ marginRight: -20 }}
+					/>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={signOut}>
+					<Icon
+						name='logout'
+						size={30}
+						color={"#ccc"}
+						style={{ marginRight: -20 }}
+					/>
+				</TouchableOpacity>
+			</View>
+			<View>
+				<DrawerItemList {...props} />
+			</View>
+		</DrawerContentScrollView>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		paddingVertical: 0,
+		paddingHorizontal: 0,
+	},
+	headerContainer: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		paddingRight: 28,
+		paddingLeft: 5,
+		marginTop: -10,
+	},
+});

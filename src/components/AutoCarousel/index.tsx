9f6bdf9136f4d56169020bb0aeeb28c4379ae6c6
humbertojr <astronomi@gmail.com>
2025-02-25 21:05:16 -0300

    Updating AutoCarousel component

--- src/components/AutoCarousel/index.tsx
+++ src/components/AutoCarousel/index.tsx
@@ -1,144 +1,287 @@
 import * as React from "react";
-import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
+import {
+	ActivityIndicator,
+	Alert,
+	Dimensions,
+	Image,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
+} from "react-native";
 import Carousel from "react-native-reanimated-carousel";
+import { useNavigation } from "@react-navigation/native";
+
+import api from "../../services/api";
 
 const width = Dimensions.get("window").width;
 
-export function AutoCarousel() {
+interface BandProps {
+	id: string;
+	name: string;
+	bandCover: string;
+	item: string | undefined;
+}
+
+interface changeBandsIntervalsProps {
+	changeBandsIntervals: any;
+}
+
+export function AutoCarousel({ genreId }) {
+	const navigation = useNavigation();
+
 	const [pagingEnabled, setPagingEnabled] = React.useState(true);
+	const [bandsId, setBandsId] = React.useState([]);
+	const [bands, setBands] = React.useState([]);
 
-	const data = [
-		{
-			id: "14",
-			name: "Carcass",
-			country: "United Kingdom",
-			location: "Liverpool, Merseyside, England",
-			formedIn: "1986",
-			yearsActive: "1986-1996, 2007-present",
-			genre: "Goregrind/Grindcore (early); Melodic Death Metal/Death 'n' Roll (later)",
-			themes: "Death, Gore, Pathology, Medical, Humour, Politics, Society",
-			label: "Nuclear Blast",
-			bandCover:
-				"https://www.metal-archives.com/images/1/4/14_photo.jpg?2430",
-		},
+	const [genreName, setGenreName] = React.useState("Death");
 
-		{
-			id: "141",
-			name: "Death",
-			country: "United States",
-			location: "Altamonte Springs, Florida",
-			formedIn: "1984",
-			yearsActive: "1983-1984 (as Mantas), 1984-2001",
-			genre: "Death Metal (early); Progressive Death Metal (later)",
-			themes: "Death, Gore, Horror (early); Society, Enlightenment (later)",
-			label: "Nuclear Blast",
-			bandCover:
-				"https://www.metal-archives.com/images/1/4/1/141_photo.jpg?5804",
-		},
-		{
-			id: "34",
-			name: "Candlemass",
-			country: "Sweden",
-			location: "Stockholm",
-			formedIn: "1985",
-			yearsActive:
-				"1982-1984 (as Nemesis), 1985-1994, 1997-2002, 2004-present",
-			genre: "Epic Doom Metal",
-			themes: "Dark fantasy, Religion, Doom, Life, Death, Hell",
-			label: "Napalm Records",
-			bandCover:
-				"https://www.metal-archives.com/images/3/4/34_photo.jpg?1351",
-		},
-		{
-			id: "151",
-			name: "My Dying Bride",
-			country: "United Kingdom",
-			location: "Bradford, West Yorkshire, England",
-			formedIn: "1990",
-			yearsActive: "1990-present",
-			genre: "Doom/Death Metal (early); Gothic/Doom Metal (later)",
-			themes: "Despair, Pain, Romance, Faithlessness, Tragedy",
-			label: "Nuclear Blast",
-			bandCover:
-				"https://www.metal-archives.com/images/1/5/1/151_photo.jpg?1948",
-		},
-		{
-			id: "68",
-			name: "Morbid Angel",
-			country: "United States",
-			location: "Tampa, Florida",
-			formedIn: "1984",
-			yearsActive: "1983(as Ice),  1983-1984 (as Heretic), 1984-present",
-			genre: "Death Metal",
-			themes: "Death, Satanism, Blasphemy, Sumerian mythology, Occultism, Ancient Ones",
-			label: "Silver Lining Music",
-			bandCover:
-				"https://www.metal-archives.com/images/6/8/68_photo.jpg?2756",
-		},
-		{
-			id: "119273",
-			name: "Crux Cullum",
-			country: "Brazil",
-			location:
-				"Belo Horizonte, Minas Gerais (early); Vila Velha, Espírito Santo (later)",
-			formedIn: "2007",
-			yearsActive: "2007-2016, 2024-present (as Mordrom)",
-			genre: "Death/Black Metal",
-			themes: "Hate, Misanthropy, Anti-religion",
-			label: "Anaites Records",
-			bandCover:
-				"https://www.metal-archives.com/images/1/1/9/2/119273_photo.jpg?5801",
-		},
-	];
+	const [isLoading, setIsLoading] = React.useState(false);
+
+	const [changeBands, setChangeBands] = React.useState(0);
+
+	React.useEffect(() => {
+		loadBand();
+	}, []);
+
+	React.useEffect(() => {
+		updatedBandsCarousel();
+	}, []);
+
+	let count = 0;
+	const updatedBandsCarousel = () => {
+		setInterval(() => {
+			count++;
+
+			loadBand();
+			console.log("------ setInterval - setInterval -----> ");
+
+			setChangeBands(count);
+		}, 60000 * 3);
+		setChangeBands(count);
+	};
+
+	console.log("----- ChangeBands ------ ", changeBands);
+
+	async function loadBand() {
+		setChangeBands(count);
+		try {
+			fetch(`https://metal-api.dev/search/bands/genre/${genreName}`, {
+				method: "GET",
+			})
+				.then(async (response) => {
+					setIsLoading(true);
+					console.log("----- get BandId ------ ");
+					const res = await response.json();
+					setBandsId([res]);
+
+					setGenreName(genreName);
+
+					// get ID bands
+					const bandIds = await res.map((item: BandProps) => {
+						item.id;
+
+						return {
+							id: item.id,
+						};
+					});
+					setBandsId(bandIds);
+					setIsLoading(false);
+
+					let i = 0;
+					const allBands = [];
+
+					if (changeBands == 0 || changeBands == null) {
+						console.log(
+							"----- ChangeBands === 000000 ------ ",
+							changeBands,
+						);
+						for (i = 0; i <= bandIds.length && i <= 10; i++) {
+							const response = await api
+								.get(`/bands/${bandIds[i].id}`)
+								.then((response) => {
+									setIsLoading(true);
+									const data = response.data;
+
+									allBands.push(data);
+
+									setIsLoading(false);
+								})
+								.catch((error) => {
+									console.error(error);
+									setIsLoading(false);
+									Alert.alert(
+										"Error",
+										"Please try again more later! 1 loadBandId",
+									);
+								});
+						}
+					} else if (changeBands != 0 || changeBands != null) {
+						console.log(
+							"----- ChangeBands !== 000000 ------ ",
+							changeBands,
+						);
+						for (
+							i = changeBands + 1;
+							i <= bandIds.length && i <= changeBands + 10;
+							i++
+						) {
+							const response = await api
+								.get(`/bands/${bandIds[i].id}`)
+								.then((response) => {
+									setIsLoading(true);
+									const data = response.data;
+
+									allBands.push(data);
+									setIsLoading(false);
+								})
+								.catch((error) => {
+									console.error(error);
+									setIsLoading(false);
+									Alert.alert(
+										"Error",
+										"Please try again more later! 1 loadBandId",
+									);
+								});
+						}
+					}
+
+					const filterBands = allBands.map((b: BandProps) => {
+						b.id, b.name, b.bandCover;
+
+						return {
+							id: b.id,
+							name: b.name,
+							bandCover: b.bandCover,
+						};
+					});
+
+					console.log("Bands filterBands -----> ", filterBands);
+
+					setBands(filterBands);
+					setIsLoading(false);
+				})
+				.catch((error) => {
+					console.error("Error then catch Fetch Bands: ", error);
+
+					Alert.alert("Error", "Please try again more later!");
+					setIsLoading(false);
+				})
+				.finally(() => {
+					console.log("finally");
+					setIsLoading(false);
+				});
+		} catch (error) {
+			console.error("Error", error);
+			setIsLoading(false);
+			Alert.alert("Error", "Please try again more later! 2 loadBandId");
+		}
+	}
+
+	React.useEffect(() => {
+		loadBand();
+	}, []);
+
+	const RenderItem = (item: BandProps) => {
+		return (
+			<TouchableOpacity
+				key={item.id}
+				onPress={() => seeMyFavoriteBand(item.id)}>
+				<View style={styles.renderItem}>
+					{item.bandCover ? (
+						<Image
+							style={styles.image}
+							resizeMode='cover'
+							source={{
+								uri: `${item.bandCover}`,
+							}}
+						/>
+					) : (
+						<Image
+							style={styles.image}
+							resizeMode='cover'
+							source={require("../../assets/images/without-image.jpg")}
+						/>
+					)}
+					<Text style={styles.title}>{item.name}</Text>
+					<Text style={styles.title}>&nbsp;</Text>
+				</View>
+			</TouchableOpacity>
+		);
+	};
+
+	function seeMyFavoriteBand(item: string) {
+		navigation.setParams({ item });
+
+		navigation.navigate("My Favorite Band Details", { item });
+
+		console.log("MyFavoriteGenres - item.id: ", item);
+	}
 
 	return (
-		<View style={{ flex: 1 }}>
-			<Carousel
-				data={data}
-				autoPlay={true}
-				pagingEnabled={pagingEnabled}
-				scrollAnimationDuration={5000}
-				width={width}
-				height={width}
-				renderItem={({ item }) => (
-					<>
-						<View style={styles.renderItem}>
-							<Image
-								source={{
-									uri: `${item.bandCover}`,
-								}}
-								resizeMode='cover'
-								style={styles.image}
-							/>
-						</View>
-						<Text style={styles.title}>{item.name}</Text>
-						<Text style={styles.text}>
-							Formed In: {item.formedIn} - {item.country}
-						</Text>
-					</>
-				)}
-			/>
+		<View style={styles.container}>
+			<Text style={styles.textBold}>
+				Bands&nbsp;{genreName}&nbsp;Metal
+			</Text>
+
+			{!isLoading ? (
+				<Carousel
+					data={bands}
+					autoPlay={true}
+					pagingEnabled={pagingEnabled}
+					scrollAnimationDuration={5000}
+					mode='parallax'
+					modeConfig={{
+						parallaxScrollingScale: 0.9,
+						parallaxScrollingOffset: 50,
+					}}
+					width={width}
+					height={width}
+					renderItem={({ item }) => <RenderItem {...item} />}
+				/>
+			) : (
+				<ActivityIndicator
+					size={"large"}
+					color='#fff'
+					style={{
+						alignItems: "center",
+						justifyContent: "center",
+						marginTop: 70,
+					}}
+				/>
+			)}
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-	renderItem: {
+	container: {
 		flex: 1,
+		backgroundColor: "#000",
+		alignItems: "center",
+		justifyContent: "flex-start",
+	},
+	renderItem: {
+		alignItems: "center",
 		justifyContent: "center",
-		overflow: "hidden",
 	},
-	image: {
-		width: width,
-		height: 300,
-	},
+	image: { width: width, height: width },
 	title: {
 		width: "100%",
+		height: 90,
 		color: "#fff",
-		backgroundColor: "#1f1d1d",
+		backgroundColor: "#000",
 		fontSize: 32,
+		padding: 5,
 		fontWeight: "bold",
-		padding: 10,
+		textAlign: "center",
+		marginLeft: 2,
+	},
+	textBold: {
+		color: "#fff",
+		fontSize: 16,
+		padding: 5,
+		fontWeight: "bold",
 		textAlign: "center",
 	},
 	text: {
@@ -146,5 +289,7 @@ const styles = StyleSheet.create({
 		fontSize: 16,
 		fontWeight: "normal",
 		textAlign: "center",
+		backgroundColor: "#1f1d1d",
+		marginLeft: 2,
 	},
 });

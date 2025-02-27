60f38430e9e8893bb6430b74abdca5445fbabda1
humbertojr <astronomi@gmail.com>
2025-02-27 10:32:40 -0300

    Updating App

--- App.tsx
+++ App.tsx
@@ -3,18 +3,12 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { StatusBar } from "react-native";
 
 import { Router } from "./src/routes/Router";
-import { AuthProvider } from "./src/contexts/Auth";
-import { ThemeProvider } from "@react-navigation/native";
 
 function App(): React.JSX.Element {
 	return (
 		<GestureHandlerRootView>
-			<AuthProvider>
-				<ThemeProvider>
-					<StatusBar barStyle={"dark-content"} translucent />
-					<Router />
-				</ThemeProvider>
-			</AuthProvider>
+			<StatusBar barStyle={"dark-content"} translucent />
+			<Router />
 		</GestureHandlerRootView>
 	);
 }

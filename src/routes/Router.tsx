2144b898844d0ce43e3adfb1cd70ec29e805e228
humbertojr <astronomi@gmail.com>
2025-02-27 10:27:04 -0300

    Updating Route navigation

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,16 +1,12 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
-import { AuthStack } from "./AuthStack";
 import { AppDrawer } from "./AppDrawer";
-import { useAuth } from "../contexts/Auth";
 
 export function Router() {
-	const { authData } = useAuth();
-
 	return (
 		<NavigationContainer>
-			{authData ? <AppDrawer /> : <AuthStack />}
+			<AppDrawer />
 		</NavigationContainer>
 	);
 }

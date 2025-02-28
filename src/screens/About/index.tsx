9b19b0fd923e60a1dcb826422244d1b9c3819d6f
humbertojr <astronomi@gmail.com>
2025-02-28 13:31:01 -0300

    Updating text

--- src/screens/About/index.tsx
+++ src/screens/About/index.tsx
@@ -5,6 +5,7 @@ import {
 	StyleSheet,
 	SafeAreaView,
 	Dimensions,
+	ScrollView,
 	Image,
 } from "react-native";
 
@@ -13,33 +14,40 @@ const windowWidth = Dimensions.get("window").width;
 export function AboutScreen() {
 	return (
 		<SafeAreaView style={styles.container}>
-			<View style={styles.containerInfo}>
-				<Text style={styles.title}>Headbanger Forever</Text>
-				<Text style={styles.text}>
-					I'm developing this app to promote metal bands of all genres
-					on the{" "}
-					<Text style={styles.metalArquives}>metal-arquives.com</Text>
-					&nbsp;website, but I'm using an unofficial API that has been
-					released and approved by the site. The Api I'm using is
-					here:{" "}
-					<Text style={styles.metalArquives}>metal-api.dev</Text>,
-					it's in its v1 version, but according to the information on
-					the site, they're already working on v2, visit and read more
-					information.
-				</Text>
-				<View
-					style={{
-						justifyContent: "center",
-						width: windowWidth,
-						alignItems: "center",
-					}}>
-					<Image
-						resizeMode='contain'
-						style={styles.emailImage}
-						source={require("../../assets/images/hand-headbanguer.png")}
-					/>
+			<ScrollView>
+				<View style={styles.containerInfo}>
+					<Text style={styles.title}>Headbanger Forever</Text>
+					<Text style={styles.text}>
+						I'm developing this app to promote metal bands of all
+						genres on the{" "}
+						<Text style={styles.metalArquives}>
+							metal-arquives.com
+						</Text>
+						&nbsp;website, but I'm using an unofficial API that has
+						been released and approved by the site. The Api I'm
+						using is here:{" "}
+						<Text style={styles.metalArquives}>metal-api.dev</Text>,
+						it's in its v1 version, but according to the information
+						on the site, they're already working on v2, visit and
+						read more information. {"\n\n"}But unfortunately in this
+						v1 the API, this group of API-Calls can be used to
+						search for bands or albums (the search is only limited
+						to the first 200 results)
+					</Text>
+					<View
+						style={{
+							justifyContent: "center",
+							width: windowWidth,
+							alignItems: "center",
+						}}>
+						<Image
+							resizeMode='contain'
+							style={styles.emailImage}
+							source={require("../../assets/images/hand-headbanguer.png")}
+						/>
+					</View>
 				</View>
-			</View>
+			</ScrollView>
 		</SafeAreaView>
 	);
 }
